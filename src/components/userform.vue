e2646884e2a9bb097737cda37590d02ceede31f7
zhoushun <941739320@qq.com>
2019-08-23 15:22:00 +0800

    增加登录页面

--- src/components/userform.vue
+++ src/components/userform.vue
@@ -10,8 +10,9 @@
 			</el-table-column>
 			<el-table-column prop="address" label="地址" show-overflow-tooltip>
 			</el-table-column>
+			<el-table-column prop="phone" label="号码" show-overflow-tooltip>
+			</el-table-column>
 		</el-table>
-
 	</div>
 </template>
 
@@ -22,31 +23,14 @@
         tableData: [{
           date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-02',
+          address: '上海市普陀区金沙江路 ',
+          phone:"15012745523"
+        },{
+          date: '2016-05-03',
           name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-08',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-06',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-07',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
+          address: '上海市普陀区金沙江路 1518 弄',
+          phone:"15012745523"
+          
         }],
         multipleSelection: []
       }
